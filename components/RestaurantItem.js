f1ed027d507a8ad4c67e8b692957c04e35d3ca30
ajayhf <ajaykumar.r@happyfox.com>
2021-10-05 15:01:28 +0530

    completion of the categories component;

---
+++ components/RestaurantItem.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+
+export default function RestaurantItem() {
+    return (
+        <TouchableOpacity activeOpacity={1} style={{marginBottom:30}}>
+            <View style={{ marginTop: 10, padding: 15, backgroundColor: 'white' }}>
+                <RestaurantImage />
+                <RestaurantInfo />
+            </View>
+        </TouchableOpacity>
+
+    )
+}
+
+const RestaurantImage = () => (
+    <>
+        <Image source={{ uri: "https://im1.dineout.co.in/images/uploads/restaurant/sharpen/2/e/d/p29410-15743151555dd62493bdda1.jpg?tr=tr:n-xlarge" }}
+            style={{ height: 180, width: '100%' }}
+
+        ></Image>
+        <TouchableOpacity style={{ position: "absolute", right: 20, top: 20 }}>
+            <MaterialCommunityIcons name="heart-outline" size={25} color="#fff" />
+        </TouchableOpacity>
+    </>
+)
+
+const RestaurantInfo = () => (
+    <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginTop: 10 }}>
+        <View>
+            <Text style={{ fontSize: 15, fontWeight: 'bold' }}>FarmHouse kitchen cusine</Text>
+            <Text style={{ fontSize: 13, color: 'gray' }}>30-45 mins</Text>
+        </View>
+        <View style={{ backgroundColor: '#eee', height: 30, width: 30, justifyContent: "center", alignItems: 'center', borderWidth: 1, borderRadius: 15 }}>
+            <Text >4.5</Text>
+        </View>
+
+    </View>
+
+)
--- screens/Home.js
+++ screens/Home.js
@@ -1,19 +1,24 @@
 import React from 'react'
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import Categoris from '../components/Categoris'
 import HeaderTabs from '../components/HeaderTabs'
+import RestaurantItem from '../components/RestaurantItem'
 import SearchBar from '../components/SearchBar'
 
 
 export default function Home() {
     return (
-        <SafeAreaView style={{backgroundColor:'#eee',flex:1}}>
-            <View style={{backgroundColor:'white',padding:15}}>
-                <HeaderTabs/> 
-                <SearchBar/>
+        <SafeAreaView style={{ backgroundColor: '#eee', flex: 1 }}>
+            <View style={{ backgroundColor: 'white', padding: 15 }}>
+                <HeaderTabs />
+                <SearchBar />
             </View>
-            <Categoris></Categoris>
-            
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <Categoris></Categoris>
+                <RestaurantItem></RestaurantItem>
+            </ScrollView>
+
+
         </SafeAreaView>
     )
 }
