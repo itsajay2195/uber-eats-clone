38dd8a4b455bb7cebd97d2561b4216b3fcc066e3
itsajay2195 <ajaykumar.95@yahoo.com>
2021-10-16 10:17:44 +0530

    adding checkout modal

--- components/restaurantDetail/ViewCart.js
+++ components/restaurantDetail/ViewCart.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, TouchableOpacity, Modal } from 'react-native'
 import { useSelector } from 'react-redux'
 
 export default function ViewCart() {
     const items = useSelector((state) => state.cartReducer.selectedItems.items)
+    const [modalVisible, setModalVisible] = useState(false)
 
     // the following breakdwon is being made in the below piece of cod
     // INR 250 will be striped to 250
@@ -15,30 +16,52 @@ export default function ViewCart() {
         .reduce((prev, cur) => prev + cur, 0)
     // items.map((item)=>console.log('price is',item.price))
     // console.warn('total is', total)
+
+    
     return (
 
         <>
-            {total ? 
-         
-            <View style={{ flex: 1, flexDirection: 'row', paddingHorizontal: '20%', alignItems: 'center', position: 'absolute', bottom: '10%', zIndex: 999 }}>
-                <View style={{ alignItems: 'center', justifyContent: 'center', width: "100%" }}>
-                    <TouchableOpacity
-                        style={{
-                            marginTop: 20,
-                            padding: 13,
-                            width: '100%',
-                            justifyContent: 'space-around',
-                            flexDirection: 'row',
-                            backgroundColor: 'black',
-                            borderRadius: 30,
-                        }}>
-                        <Text style={{ color: 'white', fontSize: 19 }}>View Cart</Text>
-                        <Text style={{ color: 'white', fontSize: 19 }}> {total} INR</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>:
+            <Modal animationType="slide" visible={modalVisible} transparent={true} onRequestClose={() => setModalVisible(false)}>
+                <View style={{flex:1}}> 
+        
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', marginTop: 30 }}>
+                <TouchableOpacity
+                    style={{
+                        padding: 10,
+                        borderRadius: 30,
+                        flexDirection: 'row',
+                        backgroundColor: 'black'
+                    }}
+                    onPress={() => setModalVisible(false)}>
+                    <Text style={{ color: 'white' }}>Checkout</Text>
+                </TouchableOpacity>
+            </View>
 
-             <></>}
+        
+    </View>
+            </Modal>
+            {total ?
+
+                <View style={{ flex: 1, flexDirection: 'row', paddingHorizontal: '20%', alignItems: 'center', position: 'absolute', bottom: '10%', zIndex: 999 }}>
+                    <View style={{ alignItems: 'center', justifyContent: 'center', width: "100%" }}>
+                        <TouchableOpacity
+                            style={{
+                                marginTop: 20,
+                                padding: 13,
+                                width: '100%',
+                                justifyContent: 'space-around',
+                                flexDirection: 'row',
+                                backgroundColor: 'black',
+                                borderRadius: 30,
+                            }}
+                            onPress={() => setModalVisible(true)}>
+                            <Text style={{ color: 'white', fontSize: 19 }}>View Cart</Text>
+                            <Text style={{ color: 'white', fontSize: 19 }}> {total} INR</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View> :
+
+                <></>}
         </>
 
 
